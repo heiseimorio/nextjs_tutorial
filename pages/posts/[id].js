a9f984ad85baff4cafbdd727411cc1c24f750fff
heiseimorio <chapupupupupu@yahoo.co.jp>
2021-02-06 17:03:20 +0900

    各投稿の詳細ページに飛べるようにリンクを設定。

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,7 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
@@ -9,14 +10,17 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <br />
-      {postData.id}
-      <br />
-      {/* {postData.date} */}
-      <Date dateString={postData.date} />
-      <br />
-      {/* mdファイルの本文を表示させるinnerHTML */}
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        
+        {/* mdファイルの本文を表示させるinnerHTML */}
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+      
     </Layout>
   )
 }
