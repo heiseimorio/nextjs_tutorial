670715d222da810dea3a96f929827bc17b267811
heiseimorio <chapupupupupu@yahoo.co.jp>
2021-02-06 15:03:13 +0900

    マークダウンファイルのコンテンツ部分を詳細ページで表示させた。

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,9 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      {/* mdファイルの本文を表示させるinnerHTML */}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
@@ -24,7 +27,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // params.id を使用して、ブログの投稿に必要なデータを取得する
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
 
   return {
     props: {
